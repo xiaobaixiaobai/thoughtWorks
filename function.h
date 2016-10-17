9c82c3bac7299786fd96eb986193c51b6fbe25a3
xiaobaixiaobai <mp_tang@126.com>
2016-10-17 15:23:35 +0800

    Create function.h

---
+++ function.h
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#incldue<string>
+using namespace std;
+
+vector<string> getResult(vector<string> strs);
+string generateSummary(string input);
+string intToStr(int num);
+int getWeekDay(int y,int m,int d);
+int getCourtNum(int x);
+int getSumCost(int weekday,int startTime,int endTime);
